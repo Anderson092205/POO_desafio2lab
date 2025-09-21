688d8cc606d206db4c02fdd893689ad0cbcaf51d
EL-25 <edwindaniel.leiva2021@gmail.com>
2025-09-20 22:13:43 -0600

    Subiendo codigo de Conexion.java

--- src/main/java/com/editorial/editorialweb/dao/Conexion.java
+++ src/main/java/com/editorial/editorialweb/dao/Conexion.java
@@ -1,4 +1,21 @@
 package com.editorial.editorialweb.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/editorial";
+    private static final String USER = "tu_usuario";
+    private static final String PASSWORD = "tu_contraseña";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
